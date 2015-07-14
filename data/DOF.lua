cbd9d4bd316b0ea73778c68b7fcafb9be30def6c
Jake Moffatt <jnbmoffa@uwaterloo.ca>
2015-07-14 11:16:04 -0400

    Depth of FIeld Scene

---
+++ data/DOF.lua
@@ -0,0 +1,29 @@
+red = gr.material({0.8, 0.1, 0.1}, {0.5, 0.5, 0.5}, 25)
+green = gr.material({0.1, 0.8, 0.1}, {0.5, 0.5, 0.5}, 25)
+blue = gr.material({0.1, 0.1, 0.8}, {0.5, 0.5, 0.5}, 25)
+
+root = gr.node('root')
+
+BALL = gr.sphere('BALL1')
+BALL:translate(300, 0, -700)
+BALL:scale(100, 100, 100)
+BALL:set_material(green)
+root:add_child(BALL)
+
+BALL = gr.sphere('BALL1')
+BALL:translate(0, 0, 0)
+BALL:scale(100, 100, 100)
+BALL:set_material(red)
+root:add_child(BALL)
+
+BALL = gr.sphere('BALL1')
+BALL:translate(-175, 0, 700)
+BALL:scale(100, 100, 100)
+BALL:set_material(blue)
+root:add_child(BALL)
+
+white_light = gr.light({100.0, 0.0, 500.0}, {1, 1, 1}, {1, 0, 0})
+
+gr.render(root, 'DOF.png', 1024, 1024,
+	  {0, 0, 1000}, {0, 0, -1}, {0, 1, 0}, 50,
+	  {0.3, 0.3, 0.3}, {white_light})
