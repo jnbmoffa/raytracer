6917006a310810fe9070586b91fd0d9c831124ea
Jake Moffatt <jnbmoffa@uwaterloo.ca>
2015-07-20 12:25:04 -0400

    Still restructuring?

--- camera.cpp
+++
@@ -1,7 +0,0 @@
-#include "camera.h"
-
-std::ostream& operator<<(std::ostream& out, const Camera& c)
-{
-  return out << "Camera[" << c.eye << ", " << c.view << ", " << c.up << ", " << c.fov <<
-  ", " << c.ApertureRadius << ", " << c.FocalDistance << "]";
-}
